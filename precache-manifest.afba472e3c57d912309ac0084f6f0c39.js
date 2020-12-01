7de95441483938d36d1dcadbc68b72951dfbe704
Yang Yang <yangyang1998@gmail.com>
2020-12-01 12:24:04 -0500

    Updates

---
+++ precache-manifest.afba472e3c57d912309ac0084f6f0c39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def9ef5c7a8163fe8cb6745a070cad6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4f7890f55a91c6c2bda",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "3cf4b751ab73f4526c1b",
+    "url": "/static/js/2.67f14fdf.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.67f14fdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f7890f55a91c6c2bda",
+    "url": "/static/js/main.b3d3672c.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "55289044e5f3ca97244a976a8b11ef58",
+    "url": "/static/media/resume.55289044.pdf"
+  },
+  {
+    "revision": "b00674c220dd64c19f0b1d04f8d8572d",
+    "url": "/static/media/yyang.b00674c2.png"
+  }
+]);
